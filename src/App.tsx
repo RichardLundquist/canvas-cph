8d03b725ec8db0531848afb218271f5c78b7ce5d
Richard Lundquist <49583921+RichardLundquist@users.noreply.github.com>
2025-10-18 13:48:45 +0200

    interval fix

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,13 @@ function App() {
 
   const apiKey = import.meta.env.VITE_DMI_API_KEY;
 
-  const url = `https://dmigw.govcloud.dk/v2/metObs/collections/observation/items?period=latest&stationId=06180&limit=100&bbox-crs=https%3A%2F%2Fwww.opengis.net%2Fdef%2Fcrs%2FOGC%2F1.3%2FCRS84&api-key=${apiKey}`;
+  const station = {
+    botaniskHave1: '05735',
+    kbhLufthavn: '06180',
+    kbhToldbod: "06187"
+  }
+
+  const url = `https://dmigw.govcloud.dk/v2/metObs/collections/observation/items?stationId=${station.kbhLufthavn}&period=latest-10-minutes&bbox-crs=https%3A%2F%2Fwww.opengis.net%2Fdef%2Fcrs%2FOGC%2F1.3%2FCRS84&api-key=${apiKey}`;
 
   const fetchData = async () => {
     try {
